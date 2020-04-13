eb4fca91f7bd6f36062122281e6a2d98d1a0d791
Alexander Okatyev <a.okatyev@bssys.com>
2020-04-13 12:54:55 +0400

    start

---
+++ src/app.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <div class="container pt-5 text-center">
+            <h1>{{title}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Counter from './components/Counter'
+    import SecondCounter from './components/SecondCounter'
+    import Actions from './components/Actions'
+
+    export default {
+
+
+        data () {
+            return {
+                counter: 0
+            }
+        },
+        components: {
+
+        },
+        computed: {
+            title() {
+                return this.$store.getters.computedTitle
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
